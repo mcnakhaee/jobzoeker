7697938781ceae24764ed3440e4268e9316f3f88
mcnakhaee <mcnakhaee@gmail.org>
2023-11-28 00:35:35 +0100

    add collect data_fix issues

--- get_data.py
+++ get_data.py
@@ -1,6 +1,7 @@
 import time
 from jobspy import scrape_jobs
-
+import pandas as pd
+search_terms = ['r','ggplot','data analyst', 'analytics engineer','data scientist','pyspark','data visualization', 'data journalist']
 #Indeed
 df_jobs = []
 for search_term in search_terms:
@@ -27,5 +28,6 @@ for search_term in search_terms:
     jobs['search_term'] = search_term
     df_jobs.append(jobs)
     time.sleep(50)
+    
 merged_df = pd.concat(df_jobs, ignore_index=True)
 merged_df.to_csv('jobs.csv', index = False)
