3f25a496b43a6e0b3b0842d0ec53595f3d3acacf
mcnakhaee <mcnakhaeee@gmail.com>
2023-11-28 13:31:19 +0100

    add main

--- get_data.py
+++ get_data.py
@@ -1,33 +1,45 @@
 import time
 from jobspy import scrape_jobs
 import pandas as pd
-search_terms = ['r','ggplot','data analyst', 'analytics engineer','data scientist','pyspark','data visualization', 'data journalist']
-#Indeed
+search_terms = ['r', 'ggplot', 'data analyst', 'analytics engineer',
+                'data scientist', 'pyspark', 'data visualization', 'data journalist']
+# Indeed
 df_jobs = []
-for search_term in search_terms:
-    jobs = scrape_jobs(
-        site_name=[ "indeed"],
-        search_term=search_term,
-        location="Netherlands",
-        results_wanted=30,
-        country_indeed='Netherlands'  # only needed for indeed / glassdoor
-    )
-    jobs['search_term'] = search_term
-    df_jobs.append(jobs)
-    time.sleep(50)
-    
-# Linkedin    
-for search_term in search_terms:
-    jobs = scrape_jobs(
-        site_name=[ "linkedin"],
-        search_term=search_term,
-        location="Netherlands",
-        results_wanted=30,
-        country_indeed='Netherlands'  # only needed for indeed / glassdoor
-    )
-    jobs['search_term'] = search_term
-    df_jobs.append(jobs)
-    time.sleep(50)
-    
-merged_df = pd.concat(df_jobs, ignore_index=True)
-merged_df.to_csv('jobs.csv', index = False)
+
+
+def collect_data():
+    for search_term in search_terms:
+        jobs = scrape_jobs(
+            site_name=["indeed"],
+            search_term=search_term,
+            location="Netherlands",
+            results_wanted=30,
+            country_indeed='Netherlands'  # only needed for indeed / glassdoor
+        )
+        jobs['search_term'] = search_term
+        df_jobs.append(jobs)
+        time.sleep(50)
+
+    # Linkedin
+    for search_term in search_terms:
+        jobs = scrape_jobs(
+            site_name=["linkedin"],
+            search_term=search_term,
+            location="Netherlands",
+            results_wanted=30,
+            country_indeed='Netherlands'  # only needed for indeed / glassdoor
+        )
+        jobs['search_term'] = search_term
+        df_jobs.append(jobs)
+        time.sleep(50)
+
+    merged_df = pd.concat(df_jobs, ignore_index=True)
+    merged_df.to_csv('jobs.csv', index=False)
+
+
+def main():
+    collect_data()
+
+
+if __name__ == "__main__":
+    main()
