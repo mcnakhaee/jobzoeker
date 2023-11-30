43b7fe30f90f7f5ef033b714985a63c435847d73
mcnakhaee <mcnakhaeee@gmail.com>
2023-11-30 15:41:09 +0100

    change layout

--- app.py
+++ app.py
@@ -23,8 +23,8 @@ def detect_lang(x):
 def get_data():
     df = pd.read_csv('jobs.csv')
     df['date_posted'] = pd.to_datetime(df['date_posted'])
-    df = df[['title', 'company', 'date_posted', 'description', 'search_term',
-             "job_url"]].sort_values(by='date_posted', ascending=False)
+    df = df[['title', 'company', 'search_term', "job_url", 'date_posted', 'description',
+             ]].sort_values(by='date_posted', ascending=False)
     df = df[~df['title'].str.contains('Intern', case=False, na=False)]
     # Apply the function to filter English rows
     df['lang'] = df['description'].apply(lambda x: detect_lang(x))
@@ -46,7 +46,7 @@ def main():
     st.sidebar.header('Select Date')
 
     # start_date = st.sidebar.date_input("Start date", min(df['date_posted']))
-    end_date = st.sidebar.date_input("End date", max(df['date_posted']))
+    end_date = st.date_input("End date", max(df['date_posted']))
     # Calculate the start and end date for the last month
     end_date = datetime.now()
     start_date = end_date - timedelta(days=end_date.day)
@@ -54,22 +54,23 @@ def main():
                      & (df['date_posted'] <= end_date)]
 
     # Sidebar - Categorical column selection
-    st.sidebar.header('Select Categorical Column')
-    cat_column = st.sidebar.selectbox(
-        'Choose a Categorical Column', df.select_dtypes(include='object').columns)
-    st.sidebar.header('Select Categorical Column')
+    # st.sidebar.header('Select Categorical Column')
+    # cat_column = st.sidebar.selectbox(
+    #    'Choose a Categorical Column', df.select_dtypes(include='object').columns)
+    # st.sidebar.header('Select Categorical Column')
     langs = filtered_df.lang.unique().tolist()
-    langs_st = st.multiselect(
+    langs_st = st.sidebar.multiselect(
         "Lang",
         langs,
         langs[0])
     filtered_df = filtered_df[filtered_df['lang'].isin(langs_st)]
     search_terms = filtered_df.search_term.unique().tolist()
-    sterms = st.multiselect(
+    sterms = st.sidebar.multiselect(
         "Search Term",
         search_terms,
         search_terms[0:2])
 
+    filtered_df = filtered_df[filtered_df['search_term'].isin(sterms)]
     """titles = filtered_df.title.unique().tolist()
     st_titles = st.multiselect(
     "Job Titles",
@@ -77,7 +78,7 @@ def main():
     titles)"""
     # Filter dataframe based on selections
     # filtered_df = df[[date_column, cat_column]]
-    # filtered_df = filtered_df[filtered_df['search_term'].isin(sterms)]
+
     # filtered_df = filtered_df[filtered_df['title'].isin(st_titles)]
     # Display filtered dataframe
     st.write('### Filtered DataFrame')
