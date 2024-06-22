6eb6a44711064132cf894e365efca53649a8fe47
mcnakhaee <mcnakhaee@gmail.org>
2024-06-22 22:00:29 +0200

    add company

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# standardize the following lines of code to use pip8 standards
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -5,13 +6,16 @@ from datetime import datetime, timedelta
 import langid
 import openai
 import os
-openai_key = api_key = os.getenv("OPENAI_KEY")
+from my_secrets import *
+
+locations = ['Den Haag','Amsterdam','Rotterdam','Delft','Utrecht','Leiden','Zuid-Holland','Werk van thuis, NL']
+try:
+    openai_key = get_openai_key()
+except:
+    openai_key = api_key = os.getenv("OPENAI_KEY")
 client = openai.OpenAI(api_key=openai_key)
 
 pd.set_option('display.max_colwidth', None)
-# Function to format URLs as clickable links
-def format_url(url):
-    return f'<a href="{url}" target="_blank">{url}</a>'
 
 # Function to truncate text in the 'Description' column
 def truncate_text(text, max_length=30):
@@ -20,36 +24,19 @@ def truncate_text(text, max_length=30):
     else:
         return text
 
-
-def detect_language(text):
-    lang, _ = langid.classify(text)
-    return lang == 'en'
-
-
-def detect_lang(x):
-    try:
-        lang, _ = langid.classify(x)
-        return lang
-    except:
-        return 'not_detected'
-
-
 def get_data():
     df = pd.read_csv('jobs.csv')
     df['date_posted'] = pd.to_datetime(df['date_posted'])
     df = df.sort_values(by='date_posted', ascending=False)
-    df = df[~df['title'].str.contains('Intern', case=False, na=False)]
-    # Apply the function to filter English rows
-    df['lang'] = df['description'].apply(lambda x: detect_lang(x))
-    df = df[df['lang'] == 'en']
     return df
 
-# Function to filter DataFrame based on user input
+
 def filter_dataframe(data_frame, column, keyword):
     if keyword:
         return data_frame[data_frame[column].str.contains(keyword, case=False)]
     else:
         return data_frame
+
 
 def get_completion(prompt, model="gpt-3.5-turbo"):
     messages = [{"role": "user", "content": prompt}]
@@ -60,16 +47,15 @@ def get_completion(prompt, model="gpt-3.5-turbo"):
     )
     return response.choices[0].message.content
 
+
 def main():
     st.title('Job Zoeker')
 
-    # Load sample data
     df = get_data()
 
-    # date_column = st.sidebar.selectbox('Choose a Date Column', df.select_dtypes(include='datetime64').columns)
     st.sidebar.header('Select Date')
     end_date = st.sidebar.date_input("End date", max(df['date_posted']))
-    # Calculate the start and end date for the last month
+
     end_date = datetime.now()
     start_date = end_date - timedelta(days=30)
     filtered_df = df[(df['date_posted'] >= start_date)
@@ -79,7 +65,7 @@ def main():
     cols_st = st.sidebar.multiselect(
         "Cols",
         cols,
-        ['title', 'date_posted', 'search_term', 'company', 'job_url', 'site', 'description','lang'])
+        ['title', 'date_posted', 'search_term', 'company','location', 'job_url', 'site', 'description', 'lang'])
     filtered_df = filtered_df[cols_st]
 
     search_terms = filtered_df.search_term.unique().tolist()
@@ -89,30 +75,30 @@ def main():
         search_terms[0:2])
 
     filtered_df = filtered_df[filtered_df['search_term'].isin(sterms)]
-
+    location_st = st.sidebar.multiselect(
+        "Location",
+        locations,
+        locations[0]
+    )
+    #filtered_df = filtered_df[filtered_df['location'].str.contains('|'.join(location_st))]
+    #filtered_df = filtered_df[filtered_df['location'].isin(location_st)]
     sites = filtered_df.site.unique().tolist()
     site_st = st.sidebar.multiselect(
         "Site",
         sites,
         sites[0])
     filtered_df = filtered_df[filtered_df['site'].isin(site_st)]
-    # language filter
-    langs = filtered_df.site.unique().tolist()
 
-    # Search bar
     filter_keyword = st.sidebar.text_input(f"Enter description Value to Filter")
 
-    # Apply filter to DataFrame
     filtered_df = filter_dataframe(filtered_df, 'description', filter_keyword)
 
     selected_row_index = st.selectbox("Select a row", filtered_df.index)
-    # Display the selected row's 'description' as a text field
     selected_description = filtered_df.loc[selected_row_index, 'description']
     description_input = st.text_area("Description", selected_description)
     st.write("**ID:**", "**" + filtered_df.loc[selected_row_index, 'title'] +
              ' at ' + filtered_df.loc[selected_row_index, 'company'] + "**")
 
-    # ChatGPT
     prompt = f"""
     This is a job description in the field of data science,
     your task is to summarize the text into the technologies needed for this job, minimum years of experience, education requrement and write each of them them into a single sentence.
@@ -125,21 +111,28 @@ def main():
 
     response = get_completion(prompt)
     st.write(response)
-    # Display DataFrame with checkboxes for row selection
+
+
+
+    selected_company = filtered_df.loc[selected_row_index, 'company']
+    company_input = st.text_area("Company", selected_company)
+
+    prompt = f"""
+    this is the name of company that posted the job, your task is to summarize the company profile, the company's mission, the company's culture and the company's values in 1 or 2 sentences.
+    ```{selected_company}```
+    """
+
+    response = get_completion(prompt)
+    st.write(response)
+
     selected_row_indices = st.multiselect("Select rows to delete", df.index)
-    # Display a button to delete selected rows
     if st.button("Delete Selected Rows"):
-        # Delete selected rows from the DataFrame
         df.drop(index=selected_row_indices, inplace=True)
-
-        # Save the updated DataFrame to the source file (e.g., CSV)
         df.to_csv("jobs.csv", index=False)
-
-        # Display a confirmation message
         st.success("Selected rows deleted successfully!")
 
-    #st.dataframe(filtered_df)
     st.table(filtered_df.style.format({'description': truncate_text}))
+
 
 if __name__ == '__main__':
     main()
--- get_data.py
+++ get_data.py
@@ -1,3 +1,5 @@
+# refactor the following code to use pip8 standards
+
 import time
 from jobspy import scrape_jobs
 import pandas as pd
@@ -14,19 +16,47 @@ df_jobs = []
 old_data = pd.read_csv('jobs.csv')
 
 
+
 def detect_language(text):
+    """
+    Detects the language of a given text.
+
+    Args:
+        text (str): The text to detect the language of.
+
+    Returns:
+        bool: True if the language is English, False otherwise.
+    """
     lang, _ = langid.classify(text)
     return lang == 'en'
 
 
 def detect_lang(x):
+    """
+    Detects the language of a given text.
+
+    Args:
+        x (str): The text to detect the language of.
+
+    Returns:
+        str: The detected language or 'not_detected' if detection fails.
+    """
     try:
         lang, _ = langid.classify(x)
         return lang
     except:
         return 'not_detected'
 
+
 def collect_data():
+    """
+    Collects job data from different sources and filters the results.
+
+    This function scrapes job data from Indeed and LinkedIn for each search term in the `search_terms` list.
+    It then merges the scraped data with the existing data from the 'jobs.csv' file.
+    The function filters the merged data based on language, keyword, and job title.
+    Finally, it saves the filtered data to 'jobs.csv' and saves all the data (including duplicates) to 'all_jobs.csv'.
+    """
     for search_term in search_terms:
         jobs = scrape_jobs(
             site_name=["indeed"],
@@ -53,24 +83,35 @@ def collect_data():
         time.sleep(60)
 
     merged_df = pd.concat(df_jobs, ignore_index=True)
-    all_jobs = pd.concat([old_data,merged_df], ignore_index=True)
-    all_jobs =all_jobs.drop_duplicates()
+    all_jobs = pd.concat([old_data, merged_df], ignore_index=True)
+    all_jobs = all_jobs.drop_duplicates()
     all_jobs['lang'] = all_jobs['description'].apply(lambda x: detect_lang(x))
     filtered_df = all_jobs[all_jobs['lang'] == 'en']
     filtered_df = filtered_df[filtered_df['description'].str.contains('Python', case=False, na=False)]
     filtered_df = filtered_df[~filtered_df['title'].str.contains('PhD', case=False, na=False)]
     filtered_df = filtered_df[~filtered_df['title'].str.contains('Manager', case=False, na=False)]
+    
     # Convert 'date' column to datetime
     filtered_df['date_posted'] = pd.to_datetime(filtered_df['date_posted'])
+    # Filter the dataframe for the last 21 days
+    last_21_days = today - timedelta(days=21)
+    filtered_df = filtered_df[filtered_df['date_posted'] >= last_21_days]
+
     # Calculate the first and last day of the last month
     first_day_last_month = (today.replace(day=1) - timedelta(days=1)).replace(day=1)
     last_day_last_month = today.replace(day=1) - timedelta(days=1)
-    filtered_df = filtered_df[(filtered_df['date_posted'] >= first_day_last_month) ]
+    filtered_df = filtered_df[(filtered_df['date_posted'] >= first_day_last_month)]
     filtered_df.to_csv('jobs.csv', index=False)
     all_jobs.to_csv('all_jobs.csv', index=False)
 
 
 def main():
+    """
+    Entry point of the script.
+
+    Calls the `collect_data()` function to collect and filter job data.
+    """
+
     collect_data()
 
 
