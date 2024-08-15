dc8d8c2b5ce4ce43c780f4d6d79e296c624fec74
Muhammad Nakhaee <mcnakhaee@gmail.com>
2024-08-15 19:26:23 +0200

    fix reading data from linkedin

--- get_data.py
+++ get_data.py
@@ -14,7 +14,7 @@ today = datetime.today()
 
 df_jobs = []
 old_data = pd.read_csv('jobs.csv')
-
+merged_df = old_data
 
 
 def detect_language(text):
@@ -59,14 +59,14 @@ def collect_data():
     """
     for search_term in search_terms:
         jobs = scrape_jobs(
-            site_name=["indeed"],
+            site_name=["indeed", "glassdoor"],
             search_term=search_term,
             location="Netherlands",
             results_wanted=40,
             country_indeed='Netherlands'  # only needed for indeed / glassdoor
         )
         jobs['search_term'] = search_term
-        df_jobs.append(jobs)
+        merged_df = pd.concat([merged_df,jobs], ignore_index=True)
         time.sleep(60)
 
     # Linkedin
@@ -79,18 +79,19 @@ def collect_data():
             country_indeed='Netherlands'  # only needed for indeed / glassdoor
         )
         jobs['search_term'] = search_term
-        df_jobs.append(jobs)
+        merged_df = pd.concat([merged_df,jobs], ignore_index=True)
         time.sleep(60)
 
-    merged_df = pd.concat(df_jobs, ignore_index=True)
-    all_jobs = pd.concat([old_data, merged_df], ignore_index=True)
-    all_jobs = all_jobs.drop_duplicates()
+    #merged_df = pd.concat(df_jobs, ignore_index=True)
+    #all_jobs = pd.concat([old_data, merged_df], ignore_index=True)
+    all_jobs = merged_df.drop_duplicates()
     all_jobs['lang'] = all_jobs['description'].apply(lambda x: detect_lang(x))
     filtered_df = all_jobs[all_jobs['lang'] == 'en']
     filtered_df = filtered_df[filtered_df['description'].str.contains('Python', case=False, na=False)]
     filtered_df = filtered_df[~filtered_df['title'].str.contains('PhD', case=False, na=False)]
     filtered_df = filtered_df[~filtered_df['title'].str.contains('Manager', case=False, na=False)]
-    
+    filtered_df = filtered_df[~filtered_df['title'].str.contains('Intern', case=False, na=False)]
+
     # Convert 'date' column to datetime
     filtered_df['date_posted'] = pd.to_datetime(filtered_df['date_posted'])
     # Filter the dataframe for the last 21 days
@@ -101,6 +102,7 @@ def collect_data():
     first_day_last_month = (today.replace(day=1) - timedelta(days=1)).replace(day=1)
     last_day_last_month = today.replace(day=1) - timedelta(days=1)
     filtered_df = filtered_df[(filtered_df['date_posted'] >= first_day_last_month)]
+    filtered_df.drop_duplicates(subset=['title', 'company'], inplace=True)
     filtered_df.to_csv('jobs.csv', index=False)
     all_jobs.to_csv('all_jobs.csv', index=False)
 
